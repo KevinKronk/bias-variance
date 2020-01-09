42d397de4b1de20e460f6d07ade23ea79fc882b6
Kevin Kronk <kjkronk@ncsu.edu>
2020-01-09 18:14:46 -0500

    Updated plots and added comments

--- bias-variance/plot.py
+++ bias-variance/plot.py
@@ -15,15 +15,21 @@ xval, yval = data['Xval'], data['yval'].flatten()
 # Test set
 xtest, ytest = data['Xtest'], data['ytest'].flatten()
 
-# plt.scatter(x, y, c='r', s=25, marker='x')
-# plt.scatter(xval, yval, c='g', s=25)
-# plt.scatter(xtest, ytest, c='b', s=25)
-# plt.xlabel('Change in water level (x)')
-# plt.ylabel('Water flowing out of the dam (y)')
-# plt.show()
+# Plot all of the data
+plt.scatter(x, y, c='r', s=25, marker='x', label="Training Data")
+plt.scatter(xval, yval, c='g', s=25, label="Validation Data")
+plt.scatter(xtest, ytest, c='b', s=25, label="Test Data")
+plt.xlabel('Change in water level (x)')
+plt.ylabel('Water flowing out of the dam (y)')
+plt.legend()
+plt.legend()
+plt.show()
 
 theta = np.zeros(x.shape[1] + 1)
 hyper_p = 0
+
+
+# Create an optimizer to run gradient descent on the cost function
 
 
 def optimize(theta, x, y, hyper_p=0):
@@ -33,10 +39,13 @@ def optimize(theta, x, y, hyper_p=0):
     return opt_theta
 
 
-# opt_theta = optimize(theta, x, y, hyper_p)
-# print(opt_theta)
-# final = compute_cost(opt_theta, x, y, hyper_p)
-# print(final)
+opt_theta = optimize(theta, x, y, hyper_p)
+print(opt_theta)
+final = compute_cost(opt_theta, x, y, hyper_p)
+print(final)
+
+
+# Plot the linear prediction on the training data
 
 
 def predict(opt_theta, x):
@@ -49,15 +58,21 @@ def plot_prediction(opt_theta, x, y):
     x_pred = np.linspace(-50, 50, 30)[:, None]
     y_pred = predict(opt_theta, x_pred)
     fig, ax = plt.subplots(1, figsize=(10, 6))
-    ax.scatter(x, y, marker='x', c='r')
-    ax.plot(x_pred, y_pred, c='b')
+    ax.scatter(x, y, marker='x', c='r', label="Training Data")
+    ax.set_xlabel('Change in water level (x)')
+    ax.set_ylabel('Water flowing out of the dam (y)')
+    ax.plot(x_pred, y_pred, c='b', label="Linear Fit")
+    ax.legend()
 
 
-# plot_prediction(opt_theta, x, y)
-# plt.show()
+plot_prediction(opt_theta, x, y)
+plt.show()
 
 
-def plot_learning_curves(x, y, xval, yval, hyper_p=0):  # I need to review this code and try myself
+# Plot the learning curves
+
+
+def plot_learning_curves(x, y, xval, yval, hyper_p=0):
     m, n = x.shape
     train_cost = np.zeros(m)
     val_cost = np.zeros(m)
@@ -87,6 +102,3 @@ opt_theta = optimize(theta, x, y, hyper_p)
 train_cost = compute_cost(opt_theta, x, y)
 val_cost = compute_cost(opt_theta, xval, yval)
 print(train_cost, val_cost)
-
-
-
